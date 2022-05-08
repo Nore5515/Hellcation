1d9b6b48cc4c1e199ba8778e98145290a10039d5
Noah <Nore5515@gmail.com>
2022-05-08 03:10:33 -0500

    changes

---
+++ Assets/BG.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG : MonoBehaviour
+{
+
+    public AudioSource bg1;
+    public AudioSource bg2;
+    public AudioSource bg3;
+
+    // private int speed = 1;
+    // private float time;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        // bg1.Play();
+    // }
+
+    // void Accelerate(){
+    //     if (speed == 1){
+    //         time = bg1.time;
+    //         bg1.Stop();
+    //         bg2.Play();
+    //         speed += 1;
+    //     }
+    //     else if (speed == 2){
+    //         bg2.Stop();
+    //         bg3.Play();
+    //         speed += 1;
+    //     }
+    // }
+    
+}
