cd6c9333cfc89b19c73ff8fe540a413a02d26fe1
Noah <Nore5515@gmail.com>
2022-05-08 00:43:25 -0500

    added green and blue colored imps

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject attributions;
+
     public void Begin(){
          SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    public void ToggleAttributions(){
+        attributions.SetActive(!attributions.activeSelf);
     }
 }
