74d905de1f9ef912bd3fa8c81766b05105731ddb
Noah <Nore5515@gmail.com>
2022-05-08 00:11:47 -0500

    player stats and upgrades ui setup

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerStats
+{
+    public static int Kills { get; set; }
+    public static int Deaths { get; set; }
+    public static int Assists { get; set; }
+    public static float Points { get; set; }
+}
